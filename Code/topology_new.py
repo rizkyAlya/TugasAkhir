6ac05eead94afd0445cf61bc4f7edd01b13831e2
Aliyah Rizky <119912678+rizkyAlya@users.noreply.github.com>
2026-02-12 10:39:17 +0700

    Refactor topology setup to use router instead of L3 switch

--- Code/topology_new.py
+++ Code/topology_new.py
@@ -6,20 +6,13 @@ from mininet.cli import CLI
 from mininet.link import TCLink
 from mininet.log import setLogLevel
 
-def L3_Core_Switch(net, switch_name, subnets):
-    """
-    Setup L3 functionality on the core switch using IP interfaces
-    subnets: dict of {zone_name: (ip, subnet)}
-    """
-    core = net.get(switch_name)
-    # Create L3 interfaces for each subnet (simulate VLAN routing)
-    for idx, (zone, (ip, mask)) in enumerate(subnets.items()):
-        iface = f'{core.name}-eth{idx}'
-        core.cmd(f'ifconfig {iface} {ip}/{mask} up')
-    core.cmd('sysctl -w net.ipv4.ip_forward=1')
-    return core
+def addRouter(net, name):
+    """Add a router host with IP forwarding enabled"""
+    r = net.addHost(name, cls=Node)
+    r.cmd('sysctl -w net.ipv4.ip_forward=1')
+    return r
 
-def CPS_topology_L3_Core():
+def CPS_topology_reachable():
     net = Mininet(controller=Controller, link=TCLink, switch=OVSSwitch)
 
     print("*** Adding controller")
@@ -39,35 +32,39 @@ def CPS_topology_L3_Core():
     switch_field = net.addSwitch('s1')
     switch_control = net.addSwitch('s2')
     switch_it = net.addSwitch('s3')
-    core_switch = net.addSwitch('s4')  # Will act as L3 switch
+    core_switch = net.addSwitch('s4')
+
+    print("*** Adding router")
+    r0 = addRouter(net, 'r0')
 
     print("*** Creating links")
     # Field Zone
     net.addLink(h1, switch_field)
     net.addLink(h2, switch_field)
     net.addLink(switch_field, core_switch)
+    net.addLink(r0, switch_field)  # connect router to Field Zone
 
     # Control Zone
     net.addLink(h3, switch_control)
     net.addLink(switch_control, core_switch)
+    net.addLink(r0, switch_control)  # connect router to Control Zone
 
     # IT Zone
     net.addLink(h4, switch_it)
     net.addLink(h5, switch_it)
     net.addLink(switch_it, core_switch)
+    net.addLink(r0, switch_it)  # connect router to IT Zone
 
     print("*** Starting network")
     net.start()
 
-    print("*** Configuring L3 Core Switch")
-    subnets = {
-        'field': ('10.0.1.1', 24),
-        'control': ('10.0.2.1', 24),
-        'IT': ('10.0.3.1', 24)
-    }
-    L3_Core_Switch(net, 's4', subnets)
+    print("*** Configuring router interfaces")
+    # Assign router IPs per subnet
+    r0.cmd('ifconfig r0-eth0 10.0.1.1/24 up')  # Field Zone
+    r0.cmd('ifconfig r0-eth1 10.0.2.1/24 up')  # Control Zone
+    r0.cmd('ifconfig r0-eth2 10.0.3.1/24 up')  # IT Zone
 
-    print("*** Setting default routes for hosts")
+    print("*** Setting default routes on hosts")
     # Field Zone
     h1.cmd('ip route add default via 10.0.1.1')
     h2.cmd('ip route add default via 10.0.1.1')
@@ -77,7 +74,7 @@ def CPS_topology_L3_Core():
     h4.cmd('ip route add default via 10.0.3.1')
     h5.cmd('ip route add default via 10.0.3.1')
 
-    print("*** Running CLI")
+    print("*** Network ready. You can test ping between hosts.")
     CLI(net)
 
     print("*** Stopping network")
@@ -85,4 +82,4 @@ def CPS_topology_L3_Core():
 
 if __name__ == '__main__':
     setLogLevel('info')
-    CPS_topology_L3_Core()
+    CPS_topology_reachable()
